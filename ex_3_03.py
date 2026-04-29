1b74f010c74af6069f34e9636cb8efb9cf77786a
tehilahalal <162914094+tehilahalal@users.noreply.github.com>
2026-04-29 12:24:41 +0300

    Fix matrix multiplication logic and compatibility check

--- ex_3_03.py
+++ ex_3_03.py
@@ -4,7 +4,7 @@ import numpy as np
 def matrix_multiply_naive(A, B):
     # check A.shape and B.shape and make sure that the matrices
     # are compatible for multiplication
-    if A.shape[1]!=B.shape[0]: # Replace with a condition that checks compatibility
+    if A.shape[1]!=B.shape[0]: 
         raise ValueError("Matrix dimensions are not compatible for multiplication")
 
     result_dtype = np.result_type(A.dtype, B.dtype)
@@ -12,8 +12,10 @@ def matrix_multiply_naive(A, B):
     result = np.zeros(result_shape, dtype=result_dtype)
 
     number_of_multiplications = 0
-    for i in range (
-        for j in range(
-            for k in range(
+    for i in range (A.shape[0])
+        for j in range(B.shape[1])
+            for k in range(A.shape[1])
+                result[i,j]+=A[i,k]*B[k,j]
+                number_of_multiplications+=1
                 
     return result, number_of_multiplications
